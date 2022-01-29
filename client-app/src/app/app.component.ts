8abb201e12206e28e59598e2cedaa5112e7c2289
rinkane <rinkaneisdead@gmail.com>
2022-01-29 23:22:28 +0900

    scssを使用できるように変更

--- client-app/src/app/app.component.ts
+++ client-app/src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'WalkEveryPath-Unko';
+  url = 'https://www.bing.com/api/maps/mapcontrol?callback=GetMap&key=[Ale2HZNS6w2T5nbYBl4V201_CO3TcnvTAPhHR73U9LJggBIyXj202av5y4ByxS_w'
 }
