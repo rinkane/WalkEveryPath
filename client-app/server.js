bb28a5245579f95c98a8a5884351c42adb21adcf
rinkane <rinkaneisdead@gmail.com>
2022-02-01 20:12:24 +0900

    express-basic-authを使用するように変更

--- client-app/server.js
+++ client-app/server.js
@@ -1,10 +1,26 @@
 const path = require('path');
 const express = require('express');
+const basicAuth = require('express-basic-auth');
+
+const correctUserName = "rinkane";
+const correctPassword = "rinkane";
 
 // サーバをインスタンス化する
 const app = express();
 
 // 以下の設定だけで dist/index.html も返せてはいる
+app.use(basicAuth({
+  challenge: true,
+  unauthorizedResponse: () => {
+    return "Unauthorized"
+  },
+  authorizer: (username, password) => {
+    const userMatch = basicAuth.safeCompare(username, correctUserName)
+    const passMatch = basicAuth.safeCompare(password, correctPassword)
+
+    return userMatch & passMatch
+  }
+}));
 app.use(express.static(`${__dirname}/dist`));
 
 // ルートへのアクセス時は念のため dist/index.html を確実に返すようにしておく
