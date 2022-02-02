52630ea8348a407b2085729216ec808863b82cc5
rinkane <rinkaneisdead@gmail.com>
2022-02-02 12:22:09 +0900

    Leafletのマーカーを使用者の位置に表示するようにした

--- client-app/src/app/map/map.component.ts
+++ client-app/src/app/map/map.component.ts
@@ -9,28 +9,46 @@ import { Coordinates } from '../shared/model/Coordinates';
   styleUrls: ['./map.component.scss'],
 })
 export class MapComponent implements OnInit {
-  map: any;
+  map?: Leaflet.Map;
   geolocation: GeolocationService;
   coordinates?: Coordinates;
   mapLoadCount: number = 0;
+  marker?: Leaflet.Marker<any>;
 
   constructor(private readonly geolocation$: GeolocationService) {
     this.geolocation = geolocation$;
     this.geolocation.subscribe((position) => {
-
       this.coordinates = new Coordinates(
         position.coords.latitude,
         position.coords.longitude
       );
 
-      this.map = Leaflet.map('map').setView(
-        [this.coordinates.latitude, this.coordinates.longitude],
-        13
-      );
-      Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(this.map);
+      const zoom = 15;
+
+      if (this.map === undefined) {
+        this.map = Leaflet.map('map').setView(
+          [this.coordinates.latitude, this.coordinates.longitude],
+          zoom
+        );
+
+        Leaflet.tileLayer(
+          'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          {
+            attribution:
+              '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          }
+        ).addTo(this.map);
+      }
+
+      if (this.marker !== undefined) {
+        this.map.removeLayer(this.marker);
+      }
+
+      this.marker = Leaflet.marker([
+        this.coordinates.latitude,
+        this.coordinates.longitude,
+      ]);
+      this.marker.addTo(this.map);
 
       this.mapLoadCount++;
     });
